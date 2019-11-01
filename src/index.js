559668f3ac6e8c593c8399f74e1fa500539ed5cc
Renan <renanwillamy2@gmail.com>
2019-11-01 11:06:14 -0300

    Something

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 import CustomButton from '../src/components/buttons/custom_button.jsx'
-import CustomButton from '../src/components/buttons/default_button.jsx'
+import DefaultButton from '../src/components/buttons/default_button.jsx'
 
 export  {
   CustomButton,
